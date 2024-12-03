ea88d1d2d26f403a0de19e91a1c32b6745e52536
bianca2408 <bianca.istrate2408@gmail.com>
2024-12-03 21:03:29 +0200

    Setup firebase deploy and postgres connection

---
+++ src/main/java/com/example/MapHive/MapHiveApplication.java
@@ -0,0 +1,13 @@
+package com.example.MapHive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapHiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapHiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MapHive/MapHiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MapHive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapHiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
