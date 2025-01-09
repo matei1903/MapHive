d2ebaed0bcbec417ac43f58211f3812cc7fd5409
Matei <mateialexandru19@yahoo.com>
2025-01-09 19:08:14 +0200

    marker bucuresti

--- frontend/src/pages/Harta.jsx
+++ frontend/src/pages/Harta.jsx
@@ -5,9 +5,9 @@ import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 const Harta = () => {
   return (
-    <MapContainer center={[45.9432, 24.9668]} zoom={6} style={{ width: "66.67%", height: "50vh", margin: "0 auto", display: "block"  }}>
+    <MapContainer center={[45.9432, 24.9668]} zoom={8} style={{ width: "66.67%", height: "50vh", margin: "0 auto", display: "block"  }}>
     <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-    <Marker position={[45.9432, 24.9668]}>
+    <Marker position={[44.4268, 26.1025]}>
       <Popup>
         Acesta este un popup pe hartă.
       </Popup>
