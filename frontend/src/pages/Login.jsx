187cb1a6906eff347d323c77b970511acb59c4f6
Matei <mateialexandru19@yahoo.com>
2025-01-15 20:20:38 +0200

    fix 2 login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -21,7 +21,6 @@ const Login = () => {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
-          "ngrok-skip-browser-warning": "true"
         },
         body: JSON.stringify(payload),
       });
