69b6cb73775e871c2c1e0f47ba6ce214095e9611
Matei <mateialexandru19@yahoo.com>
2025-01-16 23:42:08 +0200

    fix 2

--- frontend/src/pages/Harta.jsx
+++ frontend/src/pages/Harta.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from 'react';
+import React, { useEffect, useState, useRef, useCallback } from 'react';
 import axios from 'axios';
 import { MapContainer, TileLayer, Marker, useMapEvents, Popup, Polyline } from 'react-leaflet';
 import "leaflet/dist/leaflet.css";
